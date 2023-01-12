ff0fbc9754edda7ee564773ebc4ba609bec66b34
kamilakaru <karukamila@gmail.com>
2023-01-12 18:11:38 +0300

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,9 +4,11 @@
 method that determines the numbe rof minimum operations given n character
 """
 
+
 def minOperations(n):
     """
-    A function that calculates the fewest number of operations needed to result in exactly n H characters in the file.
+    A function that calculates the fewest number of operations
+    needed to result in exactly n H characters in the file.
     args: n: nunber of charcter to be deployed
     return:
             number of min operation
@@ -16,7 +18,7 @@ def minOperations(n):
     start = 0
     counter = 0
     while now < n:
-        remainder= n - now
+        remainder = n - now
         if (remainder % now == 0):
             start = now
             now += start
