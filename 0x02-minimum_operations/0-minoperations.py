454b9f08b4f1a62bc88d8ac658f0fa4e3e61382d
kamilakaru <karukamila@gmail.com>
2023-01-12 18:06:53 +0300

    Minimun Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+method that determines the numbe rof minimum operations given n character
+"""
+
+def minOperations(n):
+    """
+    A function that calculates the fewest number of operations needed to result in exactly n H characters in the file.
+    args: n: nunber of charcter to be deployed
+    return:
+            number of min operation
+    """
+
+    now = 1
+    start = 0
+    counter = 0
+    while now < n:
+        remainder= n - now
+        if (remainder % now == 0):
+            start = now
+            now += start
+            counter += 2
+        else:
+            now += start
+            counter += 1
+    return counter
